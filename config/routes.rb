b3ece75379078b44ff0f368c1be53fe4efa24c77
Julian Dobmann <julian.dobmann@gmail.com>
2015-05-31 17:31:23 +0200

    use post instead of put (since the url where the file is uploaded to will be different from the url the file will be accessable)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  put 'audio/put_here'
+  post 'audio/put_here'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
