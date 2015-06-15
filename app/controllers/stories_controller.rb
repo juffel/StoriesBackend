46f1a89a23ff7826366cf7cd9cb9f7f3c95b24c7
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-15 22:27:52 +0200

    implemented token entry point

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -8,4 +8,20 @@ class StoriesController < ApplicationController
 
   def edit
   end
+
+  def token
+    # check if came from token entry, then fetch story by token
+    if params['token_1'].present? and params['token_2'].present? and params['token_3'].present?
+      # reassemble token
+      token = ""
+      token += params['token_1']
+      token += params['token_2']
+      token += params['token_3']
+      token = token.upcase
+
+      # TODO catch if no such id is present
+      story_id = Story.where(token: token).first.id
+      redirect_to action: "edit", id: story_id
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   post 'audio/put_here'
   get 'audio/download/:id', to: 'audio#download'
+  get 'token', to: 'stories#token'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
