f0eccd763b949b49b9f210a54523b2116a5d102a
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-16 17:22:24 +0200

    Einfache Navigation überarbeitet.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'audio/download/:id', to: 'audio#download'
   get 'token', to: 'stories#token'
 
+  root 'stories#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
