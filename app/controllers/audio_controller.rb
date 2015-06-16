b3f3d4b558676852f292611823fb4d0b0bcd9663
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-16 16:16:12 +0200

    Speicherung der statischen Aufnahmen gefixt.

--- app/controllers/audio_controller.rb
+++ app/controllers/audio_controller.rb
@@ -6,19 +6,19 @@ class AudioController < ApplicationController
 
     # also save audio file in filesystem so it can be served statically
     # file access src: http://stackoverflow.com/a/1678388/1870317
-    name = "story_" + story.id + ".3gp"
-    directory = "public/stories"
+    name = "story_" + story.id.to_s + ".3gp"
+    directory = "public/audios"
     path = File.join(directory, name)
-    File.open(path, "wb") { |f| f.write(audio.read) }
+    File.open(path, "wb") { |f| f.write(audio.read) }
 
     # set response as (probably invalid) xml
     xml = story.token
 
-    render :xml => xml, :status => 200
-  end
+    render :xml => xml, :status => 200 
+  end 
 
   def download
     content = Story.find(params[:id]).audio
     send_data content, :filename=>'recording.3gp', :type=>'application/audio', :disposition=>'attachment'
-  end
+  end 
 end
