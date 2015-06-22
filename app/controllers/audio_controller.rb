27a5b15e3e667d5cdb1f4bba9ccf012f2ca9dd49
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-22 19:15:52 +0200

    Download Methode enfernt, nicht mehr notwendig, da die Dateien jetzt status geservt werden.

--- app/controllers/audio_controller.rb
+++ app/controllers/audio_controller.rb
@@ -15,9 +15,4 @@ class AudioController < ApplicationController
 
     render :xml => xml, :status => 200
   end
-
-  def download
-    content = Story.find(params[:id]).audio
-    send_data content, :filename=>'recording.3gp', :type=>'application/audio', :disposition=>'attachment'
-  end
 end
--- app/helpers/audio_helper.rb
+++ app/helpers/audio_helper.rb
@@ -1,5 +1,5 @@
 module AudioHelper
-  def self.download_path
-    "/audio/download/"
+  def self.download_path id
+    "/audios/" + id.to_s + ".3gp"
   end
 end
