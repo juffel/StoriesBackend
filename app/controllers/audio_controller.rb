e25d57f981f25ab9f6103c09335f6a9df3478dec
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-24 11:32:17 +0200

    Server converts uploaded 3gp files to mp3.

--- app/controllers/audio_controller.rb
+++ app/controllers/audio_controller.rb
@@ -5,8 +5,13 @@ class AudioController < ApplicationController
 
     # save audio file in filesystem so it can be served statically
     # file access src: http://stackoverflow.com/a/1678388/1870317
-    file_path = Rails.root.join('public', 'audios', story.id.to_s + '.aac')
-    File.open(file_path, 'wb') { |f| f.write(audio.read) }
+    file_path_3gp = 'public/audios/'+story.id.to_s + '.3gp'
+    file_path_mp3 = 'public/audios/'+story.id.to_s + '.mp3'
+    File.open(file_path_3gp, 'wb') { |f| f.write(audio.read) }
+
+    # convert to mp3
+    # ffmpeg -i in.3gp -c:a libmp3lame output.mp3
+    system "ffmpeg -i #{file_path_3gp} -c:a libmp3lame #{file_path_mp3}"
 
     # set response as (probably invalid) xml
     xml = story.token
