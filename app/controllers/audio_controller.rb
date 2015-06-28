ea860888a76e208e0c622372cf265dd6bb7192b4
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-28 22:43:50 +0200

    download paths adjusted to wav

--- app/controllers/audio_controller.rb
+++ app/controllers/audio_controller.rb
@@ -6,12 +6,12 @@ class AudioController < ApplicationController
     # save audio file in filesystem so it can be served statically
     # file access src: http://stackoverflow.com/a/1678388/1870317
     file_path_3gp = 'public/audios/'+story.id.to_s + '.3gp'
-    file_path_mp3 = 'public/audios/'+story.id.to_s + '.mp3'
+    file_path_wav = 'public/audios/'+story.id.to_s + '.mp3'
     File.open(file_path_3gp, 'wb') { |f| f.write(audio.read) }
 
     # convert to mp3
     # ffmpeg -i in.3gp -c:a libmp3lame output.mp3
-    system "ffmpeg -i #{file_path_3gp} -c:a libmp3lame #{file_path_mp3}"
+    system "ffmpeg -i #{file_path_3gp} -c:a pcm_s32le #{file_path_wav}"
 
     # set response as (probably invalid) xml
     xml = story.token
--- app/helpers/audio_helper.rb
+++ app/helpers/audio_helper.rb
@@ -1,9 +1,12 @@
 module AudioHelper
   def self.download_path_mp3 id
-    download_path(id) + ".wav"
+    download_path(id) + ".mp3"
   end
   def self.download_path_3gp id
     download_path(id) + ".3gp"
+  end
+  def self.download_path_wav id
+    download_path(id) + ".wav"
   end
 
   private
