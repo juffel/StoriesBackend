b12a3772367bee4f2f36a4bc97de74fac827efb2
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-28 22:46:23 +0200

    forgot one mp3->wav conversion

--- app/controllers/audio_controller.rb
+++ app/controllers/audio_controller.rb
@@ -6,7 +6,7 @@ class AudioController < ApplicationController
     # save audio file in filesystem so it can be served statically
     # file access src: http://stackoverflow.com/a/1678388/1870317
     file_path_3gp = 'public/audios/'+story.id.to_s + '.3gp'
-    file_path_wav = 'public/audios/'+story.id.to_s + '.mp3'
+    file_path_wav = 'public/audios/'+story.id.to_s + '.wav'
     File.open(file_path_3gp, 'wb') { |f| f.write(audio.read) }
 
     # convert to mp3
