1c2ed41a41840c50dbe3e359134e54096f61f19b
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-29 00:16:04 +0200

    fixed absolute paths in audio controller

--- app/controllers/audio_controller.rb
+++ app/controllers/audio_controller.rb
@@ -5,8 +5,8 @@ class AudioController < ApplicationController
 
     # save audio file in filesystem so it can be served statically
     # file access src: http://stackoverflow.com/a/1678388/1870317
-    file_path_3gp = 'public/audios/'+story.id.to_s + '.3gp'
-    file_path_mp3 = 'public/audios/'+story.id.to_s + '.mp3'
+    file_path_3gp = Rails.root.to_s + '/public/audios/' + story.id.to_s + '.3gp'
+    file_path_mp3 = Rails.root.to_s + '/public/audios/' + story.id.to_s + '.mp3'
     File.open(file_path_3gp, 'wb') { |f| f.write(audio.read) }
 
     # convert to mp3
