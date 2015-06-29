f50f77e489eef3f0094c8c877d75ea0fb716f925
Julian Dobmann <julian.dobmann@gmail.com>
2015-06-29 15:53:36 +0200

    story import with title and text-body working

--- app/controllers/concerns/zlb_import.rb
+++ app/controllers/concerns/zlb_import.rb
@@ -3,19 +3,46 @@ module ZLBImport
   def self.import
     stories_json = get_stories_json
 
+    # TODO remove this statement, only for development purposes
+    Story.all.each do |s|
+      if s.zlb_id.present?
+       s.destroy
+      end
+    end
+    # END TODO
+
+    # filter for item_type 26 "Geschichte BGG" (http://grossstadtgeschichten-berlin.de/api/item_types/26)
     stories_json.each do |json|
-      id = json['id']
-      # add story if this story is not already in database
-      unless Story.where(zlb_id: id).exists?
-        story = Story.new
-        # add attributes
-        story.zlb_id = id
-        story.save
+      if json['item_type']['id'] == 26
+        id = json['id']
+
+        # add story if this story is not already in database
+        unless Story.where(zlb_id: id).exists?
+          story = Story.new
+          # add attributes
+          story.zlb_id = id
+
+          texts = json['element_texts']
+
+          # the associated text elements are not ordered by id/type but in a linear
+          # fashion with type_ids contained in child nodes, so every text element
+          # has to be scanned for their type_ids
+          texts.each do |text|
+            type = text['element']['id']
+            content = text['text']
+            case type
+            when 50
+              story.title = content
+            when 79
+              story.text = content
+            end
+          end
+          story.save
+        end
       end
     end
   end
 
-  private
   # retrieves the json data from 'http://grossstadtgeschichten-berlin.de/api/items'
   def self.get_stories_json
     require 'net/http'
